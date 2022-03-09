d94c693c72304d97c65b277733ac04bb652c4f24
Shamit <shamit.singh61@gmail.com>
2022-03-09 16:51:07 +0530

    post added and displayed by prepopulating user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
 
-    console.log(req.cookies);
-    
-    return res.render('home', {
-        title: 'Home'
-    });
+    // console.log(req.cookies);
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: 'Home',
+            posts: posts
+        });
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,8 @@ router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
+// router.post('/post', usersController.post);
+
 // use passport as a middleware to authenticate
 router.post('/create-session', passport.authenticate(
     'local',
