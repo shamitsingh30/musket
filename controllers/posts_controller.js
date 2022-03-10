82d8c72bf4766fe1bd700bc13ab68ac6cff3bfe9
Shamit <shamit.singh61@gmail.com>
2022-03-10 18:21:01 +0530

    post deleted eith commment under it

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
+const { localsName } = require('ejs');
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -8,5 +10,18 @@ module.exports.create = function(req, res){
         if(err){console.log('error in creating a post'); return;}
 
         return res.redirect('back');
+    });
+};
+
+module.exports.destroy = function(req, res){
+    console.log(req.params);
+    Post.findById(req.params.id, function(err, post){
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
     });
 };
