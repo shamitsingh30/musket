206fdda717aa0657ef2afd304b6d31153e6dd916
Shamit <shamit.singh61@gmail.com>
2022-03-17 17:32:32 +0530

    async-await added to the code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 
     // console.log(req.cookies);
     // Post.find({}, function(err, posts){
@@ -11,23 +11,28 @@ module.exports.home = function(req, res){
     //     });
     // });
 
-    //populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+    try{
+        //populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: 'Codeial',
-                posts: posts,
-                all_users: users
-            });
-        })
-    })
+        let users = await User.find({});
+
+
+        return res.render('home', {
+            title: 'Codeial',
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log('Error', err);
+    }
+    
+    
 }
