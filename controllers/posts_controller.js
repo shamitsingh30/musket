123d8e4a570d24b44f5c3e1b3ae721bf1a1771d3
Shamit <shamit.singh61@gmail.com>
2022-03-18 17:10:17 +0530

    flash messages shown

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,8 +7,10 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success', 'Post published')
         return res.redirect('back');
     }catch(err){
+        req.flash('error', err);
         return res.redirect('back');
     }
 };
@@ -21,14 +23,17 @@ module.exports.destroy = async function(req, res){
         if(post.user == req.user.id){
             post.remove();
 
-            await Comment.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            });
+            await Comment.deleteMany({post: req.params.id});
+            
+            req.flash('success', 'Post and associated comments destroyed!');
+            return res.redirect('back');
         }else{
+            req.flash('error', 'You cannot delete this post');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
         
 };
